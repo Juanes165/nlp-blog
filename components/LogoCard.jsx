a3981a92b2c2bc7ff5f855bbe01c8760baf6a95c
Juan Esteban <juan.betancourt.narvaez@correounivalle.edu.co>
2023-06-08 15:13:38 -0500

    Blog base, primera versión para producción

---
+++ components/LogoCard.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const LogoCard = () => {
+
+    return (
+        <div className="bg-white shadow-lg rounded-lg p-6 mb-8">
+            <img
+                // alt={"logo"}
+                className="align-middle"
+                src={"/logo6.png"}
+            />
+        </div>
+    )
+}
+
+export default LogoCard;
